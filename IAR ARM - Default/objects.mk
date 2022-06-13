d9754bbf28e04526e73c4e312867939bbb3d4f9b
kidsky <kidsky@jnc-tec.com.tw>
2022-06-13 14:51:18 +0800

    忽略部份檔案追蹤 /IAR ARM - Default /iar/Debug /iar/settings /iar/*.dep

--- IAR ARM - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := D:/Work/Silicon-BLE/JNC-BT-Mesh/JNC-BT-Mesh/protocol/bluetooth/lib/EFR32XG13X/IAR/binapploader.o D:/Work/Silicon-BLE/JNC-BT-Mesh/JNC-BT-Mesh/protocol/bluetooth/lib/EFR32XG13X/IAR/libbluetooth_mesh.a D:/Work/Silicon-BLE/JNC-BT-Mesh/JNC-BT-Mesh/platform/radio/rail_lib/autogen/librail_release/librail_config_bgm13p32f512ga_iar.a D:/Work/Silicon-BLE/JNC-BT-Mesh/JNC-BT-Mesh/platform/emdrv/nvm3/lib/libnvm3_CM4_iar.a D:/Work/Silicon-BLE/JNC-BT-Mesh/JNC-BT-Mesh/platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg13_iar_release.a
-
